ff8289f962016f059b417a4ad1dc9aa21fab7438
rifkan <mohamedrifkan00127@gmail.com>
2022-07-19 09:21:25 +0530

    fixed error handler middleware bug

--- server/middlewares/errorHandler.ts
+++ server/middlewares/errorHandler.ts
@@ -1,39 +1,17 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
-class CustomError {
-  message!: string;
-  status!: number;
+function errorHandler(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  additionalInfo!: any;
-
-  // eslint-disable-next-line @typescript-eslint/no-inferrable-types, @typescript-eslint/no-explicit-any
-  constructor(message: string, status: number = 500, additionalInfo: any = {}) {
-    this.message = message;
-    this.status = status;
-    this.additionalInfo = additionalInfo;
-  }
-}
-
-function handleError(
-  err: TypeError | CustomError,
+  error: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  let customError = err;
-
-  if (!(err instanceof CustomError)) {
-    customError = new CustomError(
-      'Oh no, this is embarrasing. We are having troubles my friend'
-    );
-  }
-
-  // we are not using the next function to prvent from triggering
-  // the default error-handler. However, make sure you are sending a
-  // response to client to prevent memory leaks in case you decide to
-  // NOT use, like in this example, the NextFunction .i.e., next(new Error())
-  res.status((customError as CustomError).status).send(customError);
+  res.status(error.status || 500);
+  res.send({
+    error: true,
+    message: error.message || 'Internal server error',
+  });
   next();
 }
-
-export default handleError;
+export default errorHandler;
