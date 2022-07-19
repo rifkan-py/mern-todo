ee708d0cbf9c9ba8de7ffa31af5b4bb3fe4c6204
rifkan <mohamedrifkan00127@gmail.com>
2022-07-19 09:22:19 +0530

    utility function for hashing password

---
+++ server/utils/hashPassword.ts
@@ -0,0 +1,9 @@
+import bcrypt from 'bcryptjs';
+
+async function hashPassword(password: string) {
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+}
+
+export default hashPassword;
