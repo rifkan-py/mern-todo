e425c1c9870484c65a47c3637a25413703712481
rifkan <mohamedrifkan00127@gmail.com>
2022-07-19 09:40:58 +0530

    added validPassword function

--- server/utils/hashPassword.ts
+++ server/utils/hashPassword.ts
@@ -1,9 +1,24 @@
 import bcrypt from 'bcryptjs';
+import { IUser } from '../models/User';
 
 async function hashPassword(password: string) {
-  const salt = await bcrypt.genSalt();
-  const hashedPassword = await bcrypt.hash(password, salt);
-  return hashedPassword;
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+  } catch (error) {
+    throw new Error('oopos... something went wrong');
+  }
 }
 
-export default hashPassword;
+async function isValidPassword(
+  password: string,
+  { password: userPassword }: IUser
+) {
+  try {
+    return await bcrypt.compare(password, userPassword);
+  } catch (error) {
+    throw new Error('oopos... something went wrong');
+  }
+}
+export { hashPassword, isValidPassword };
